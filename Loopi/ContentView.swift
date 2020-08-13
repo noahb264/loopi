bc644ccd59266a419fa6af41f4198ab96119164a
Noah Burkhardt <noahburk@terpmail.umd.edu>
2020-08-13 13:57:02 -0400

    Initial Commit

---
+++ Loopi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loopi
+//
+//  Created by Noah Burkhardt on 8/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loopi/LoopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopiApp.swift
+//  Loopi
+//
+//  Created by Noah Burkhardt on 8/13/20.
+//
+
+import SwiftUI
+
+@main
+struct LoopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
